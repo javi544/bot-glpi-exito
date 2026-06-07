8857906b039243494604185b5113a9ebe430aa78
Javier Trujillo <javi544.jt@gmail.com>
2026-06-07 14:54:35 -0500

    chore: dejar solo sistema IA y PMBOK

--- Iniciar bot_Todas las regionales.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title BOT GLPI - Todas las Regionales
-color 0A
-cd /d C:\Bot_Incidentes
-python bot_prueba.py
-pause
--- iniciar_bots.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\Bot_Incidentes
-
-echo Iniciando Bot Monitor...
-start "Bot" py bot.py
-
-echo Bots iniciados correctamente
